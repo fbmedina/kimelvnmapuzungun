b955e0c4f77ef12edee23a1ab7dcf9773495902c
Francisca Medina Concha <fbmedina@uc.cl>
2018-07-05 21:27:10 -0400

    displaying content in spanish and mapudungun and audio file

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -5,6 +5,47 @@ $(document).ready(function()
         var randomClass = getRandomClass();
         $(e.target).attr("class", randomClass);
     });
+
+
+    var audioElement = document.getElementById('audio1');
+    $('#stop').hide();
+
+    $('#play').click(function() {
+        $('#play').hide();
+        $('#stop').show();
+        audioElement.play();
+    });
+
+
+    $('#stop').click(function() {
+        $('#play').show();
+        $('#stop').hide();
+        audioElement.pause();
+    });
+
+    $("#mapudungun i.for-display").click(function () {
+        $("#mapudungun").removeClass("displaying").addClass("no-displaying fadeIn");
+        $("#espanol").removeClass("no-displaying").addClass("displaying fadeIn"); 
+        $("i.for-display").toggleClass('fa-rotate-90 normal');      
+    });
+
+    $("#espanol i.for-display").click(function () {
+        $("#espanol").removeClass("displaying").addClass("no-displaying fadeIn");
+        $("#mapudungun").removeClass("no-displaying").addClass("displaying fadeIn");
+        $("i.for-display").toggleClass('fa-rotate-90 normal');    
+    });
+
+    $("#saludo-mapudungun i.for-display").click(function () {
+        $("#saludo-mapudungun").removeClass("displaying").addClass("no-displaying fadeIn");
+        $("#saludo-espanol").removeClass("no-displaying").addClass("displaying fadeIn"); 
+        $("i.for-display").toggleClass('fa-rotate-90 normal');      
+    });
+
+    $("#saludo-espanol i.for-display").click(function () {
+        $("#saludo-espanol").removeClass("displaying").addClass("no-displaying fadeIn");
+        $("#saludo-mapudungun").removeClass("no-displaying").addClass("displaying fadeIn");
+        $("i.for-display").toggleClass('fa-rotate-90 normal');    
+    });
 });
 
 function getRandomClass()
